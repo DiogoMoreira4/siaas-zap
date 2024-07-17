474f19f093aa1e3b25d3b02e3cf6faca968fc722
DiogoMoreira4 <diogo.silva2607@gmail.com>
2024-07-17 10:51:06 +0100

    Zap configs from the server

--- zap_service.py
+++ zap_service.py
@@ -11,6 +11,8 @@ import logging
 import psutil
 import signal
 import siaas_aux
+import sys
+import requests
 
 logging.basicConfig(filename='zap_manager.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
@@ -32,27 +34,31 @@ def read_targets(file_path):
     config = configparser.ConfigParser(interpolation=None)
     config.read(file_path)
     targets = {}
+    required_fields = ['name', 'url']
     for section in config.sections():
         items = dict(config.items(section))
+        if not all(field in items for field in required_fields):
+            logging.error(f"Error: Target '{section}' doesn't contain the required fields to be analized. Required fields: {', '.join(required_fields)}.")
+            sys.exit(1)
         auth_required_fields = ['username', 'password', 'loginpage']
         items['has_auth'] = all(field in items for field in auth_required_fields)
         targets[section] = items
     return targets
 
 
-def start_zap_instance(port, directory):
+def start_zap_instance(port, api_key, directory):
     
     if not os.path.exists(directory):
         os.makedirs(directory)
         # Set ownership and permissions
         shutil.chown(directory, user=USER, group=USER)
         os.chmod(directory, 0o755)
-    command = f"{HOME_DIR}/zaproxy/ZAP_2.15.0/zap.sh -daemon -port {port} -dir {directory} -config api.key=123456789"
+    command = f"{HOME_DIR}/zaproxy/ZAP_2.15.0/zap.sh -daemon -port {port} -dir {directory} -config api.key={api_key}"
     process = subprocess.Popen(command.split())
     return process
 
 
-def modify_automation_plan(template_with_auth_path, template_without_auth_path, output_path, target):
+def modify_automation_plan(template_with_auth_path, template_without_auth_path, output_path, target, automationConfigs):
     if target['has_auth']:
         template_path = template_with_auth_path
     else:
@@ -117,8 +123,8 @@ def run_scan(zap, plan_path, session_name):
     zap.automation.run_plan(plan_path)
     
     
-def wait_for_zap_start(port, timeout=180):
-    zap = ZAPv2(apikey='123456789', proxies={'http': f'http://localhost:{port}', 'https': f'http://localhost:{port}'})
+def wait_for_zap_start(port, api_key, timeout=180):
+    zap = ZAPv2(apikey=api_key, proxies={'http': f'http://localhost:{port}', 'https': f'http://localhost:{port}'})
     for _ in range(timeout):
         try:
             if zap.core.version:
@@ -155,22 +161,26 @@ def collect_results(zap, context_name):
     return results
 
 
+
+
 class ZAPManager:
     def __init__(self, targets_file):
         config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'conf', 'config.ini')
         self.targets_file = targets_file
-        self.base_port = read_config(config_file, 'ZAPConfig', 'base_port')
-        self.base_dir = "/home/siaas-test/siaas-zap/instances/instance"
-        self.pid_file = read_config(config_file, 'ZAPConfig', 'pid_file')
-        self.api_uri = read_config(config_file, 'APIconfig', 'api_uri')
-        self.api_user = read_config(config_file, 'APIconfig', 'api_user')
-        self.api_password = read_config(config_file, 'APIconfig', 'api_pwd')
-        self.api_ssl_ignore = read_config(config_file, 'APIconfig', 'api_ssl_ignore_verify')
-        self.api_ssl_ca_bundle = read_config(config_file, 'APIconfig', 'api_ssl_ca_bundle')
+        self.base_dir = SIAAS_ZAP_DIR+"/instances/instance"
+        self.api_uri = read_config(config_file, 'APIConfig', 'api_uri')
+        self.api_user = read_config(config_file, 'APIConfig', 'api_user')
+        self.api_password = read_config(config_file, 'APIConfig', 'api_pwd')
+        self.api_ssl_ignore = read_config(config_file, 'APIConfig', 'api_ssl_ignore_verify')
+        self.api_ssl_ca_bundle = read_config(config_file, 'APIConfig', 'api_ssl_ca_bundle')
         self.targets = read_targets(targets_file)
         self.zap_instances = {}
         self.current_target = None
+        self.pid_file = "/tmp/zap_manager.pid"
+        self.zap_config = siaas_aux.get_request_to_server("https://127.0.0.1/api/siaas-server/siaas-zap/config", ignore_ssl=True, ca_bundle=self.api_ssl_ca_bundle, api_user=self.api_user, api_pwd=self.api_password)
         self._create_pid_file()
+        
+        print(self.api_uri, self.api_user, self.api_password)
 
     def _create_pid_file(self):
         try:
@@ -193,19 +203,19 @@ class ZAPManager:
             for idx, target_key in enumerate(target_keys):
                 target = self.targets[target_key]
                 self.current_target = target  # Atualize o alvo atual
-                port = int(self.base_port) + idx
+                port = int(self.zap_config['ZAPConfig']['base_port']) + idx
                 directory = f"{self.base_dir}{idx}"
-                process = start_zap_instance(port, directory)
+                process = start_zap_instance(port, self.zap_config['ZAPConfig']['api_key'], directory)
                 self.zap_instances[target['name']] = {"port": port, "directory": directory}
                 logging.info(f"Instance {idx} created to scan {target['name']}")
           
                 
                 # Start the scan
-                zap = wait_for_zap_start(port)
+                zap = wait_for_zap_start(port, self.zap_config['ZAPConfig']['api_key'])
                 logging.info(f"Connected to the instance {idx}")
                 plan_path = f"{directory}/automation_plan.yaml"
                 pathtoplans = os.path.dirname(os.path.abspath(__file__))
-                modify_automation_plan(f"{pathtoplans}/PlanWithAuth.yaml",f"{pathtoplans}/PlanWithoutAuth.yaml" ,plan_path, target)
+                modify_automation_plan(f"{pathtoplans}/PlanWithAuth.yaml",f"{pathtoplans}/PlanWithoutAuth.yaml" ,plan_path, target, self.zap_config['AutomationPlansConfig'])
                 logging.info("Automation Plan modified and ready!")
                 session_name = f"session_{target['name']}_{port}"
                 run_scan(zap, plan_path, session_name)
@@ -217,7 +227,7 @@ class ZAPManager:
                 results = collect_results(zap, target['name'])
                 #save_to_mongodb(results, self.db)
                 #siaas_aux.insert_in_mongodb_collection(self.collection, results)
-                siaas_aux.post_request_to_server(self.api_uri+"/siaas-server/siaas-zap/results", results, ignore_ssl=True, ca_bundle=self.api_ssl_ca_bundle, api_user=self.api_user, api_pwd=self.api_password)
+                siaas_aux.post_request_to_server(f"{self.api_uri}/siaas-server/siaas-zap/results", results, ignore_ssl=True, ca_bundle=self.api_ssl_ca_bundle, api_user=self.api_user, api_pwd=self.api_password)
                 logging.info(f"Enviei um request para a API com os resultados do {target['name']}!!!!!")
                 logging.info(f"Https Request with scan results of {target['name']} sent to the server API!")
                 del self.zap_instances[target['name']]
